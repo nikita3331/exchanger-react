9bf59c7ac932d718925fa4a52499a5c90d20e3c0
Magdalena Zulewska <madziapernach@gmail.com>
2022-10-19 19:05:43 +0200

    hook useCurrentDate added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,26 +1,8 @@
 import { Container } from "./styled.js";
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "../useCurrentDate";
 
 const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  const dateContent = date.toLocaleDateString(undefined, {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  });
-
-  const timeContent = date.toLocaleTimeString();
+  const { dateContent, timeContent } = useCurrentDate();
 
   return (
     <Container>
